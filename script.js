216cb275696122e368e9ec070b691ed964730cc5
Kanhaiyaa <128280779+Kanhaiyaa@users.noreply.github.com>
2023-12-23 11:54:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded'), function () {
+	const colorBoard=document.getElementById('color-board')
+
+	for (let i = 0; i < 800; i++) {
+		const box=document.createElement('div')
+		box.classList.add('square')
+
+		box.addEventListener('mouseover', function () {
+			box.style.backgroungColor='#e74c3c'
+		})
+
+		box.addEventListener('mouseout' ,function () {
+			setTimer(() => {
+				bax.style.bacgroundColor='#ccc'
+			},1000)
+		})
+		colorBoard.appendChild(box)
+	}
+}
